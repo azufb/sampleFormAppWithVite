5e372a20406ca8b3b4f413a779c8a65102a1538c
azufb <curiousazunas@gmail.com>
2022-11-26 20:04:35 +0900

    一般的なフォームを構成

--- src/components/SampleForm.tsx
+++ src/components/SampleForm.tsx
@@ -1,6 +1,40 @@
+import { useForm } from "react-hook-form";
+
 const SampleForm = () => {
+    const { register, handleSubmit } = useForm();
+
+    const submitForm = (data: any): void => {
+        console.log(data);
+    };
+
     return (
-        <div></div>
+        <div>
+            <p>一般的なフォーム</p>
+            <form>
+                <label>ラベル：</label>
+                <input {...register('label')} />
+                <label>ラベル：</label>
+                <select {...register('select')}>
+                    <option>選択してください</option>
+                    <option value='aaa'>aaa</option>
+                    <option value='bbb'>bbb</option>
+                    <option value='ccc'>ccc</option>
+                </select>
+                <label>ラベル：</label>
+                <input type='checkbox' value='aaa' {...register('checkbox')} />aaa
+                <input type='checkbox' value='bbb' {...register('checkbox')} />bbb
+                <input type='checkbox' value='ccc' {...register('checkbox')} />ccc
+
+                <label>ラベル：</label>
+                <input type='radio' value='girl' {...register('radio')} checked />Girl
+                <input type='radio' value='boy' {...register('radio')} />Boy
+
+                <label>ラベル：</label>
+                <textarea {...register('textarea')} />
+
+                <input type='submit' onClick={handleSubmit(submitForm)} />
+            </form>
+        </div>
     );
 };
 

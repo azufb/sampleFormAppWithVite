fdf7ef43d9b640a5283daf1c4f8819bf2a64a160
azufb <curiousazunas@gmail.com>
2022-11-27 14:18:49 +0900

    動的フォーム作成

--- src/components/SampleDynamicForm.tsx
+++ src/components/SampleDynamicForm.tsx
@@ -1,6 +1,35 @@
+import { useForm, useFieldArray } from "react-hook-form";
+
 const SampleDynamicForm = () => {
+    const { register, handleSubmit, control } = useForm({
+        defaultValues: {
+          sample: [
+            {
+                title: ''
+            }
+          ] 
+        }
+    });
+    const { fields, prepend, append, remove, replace, swap } = useFieldArray({
+        control,
+        name: 'sample'
+    });
+
     return (
-        <div></div>
+        <div>
+            <form>
+            <button type="button" onClick={() => prepend({title: ''})}>先頭に追加</button>
+                {fields.map((field: any, index: number) => (
+                    <div key={field.id}>
+                        <label>タスク：{index}</label>
+                        <input {...register(`sample.${index}.title`)} />
+                        <button type="button" onClick={() => remove(index)}>消去</button>
+                    </div>
+                ))}
+                <button type="button" onClick={() => replace({title: 'You Can Do It!'})}>置き換え</button>
+                <button type="button" onClick={() => append({title: ''})}>後ろに追加</button>
+            </form>
+        </div>
     );
 };
 

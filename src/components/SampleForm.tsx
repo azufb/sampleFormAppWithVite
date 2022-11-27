9fefe727e91ae98b44b6ba9124ad7bc9d3680e45
azufb <curiousazunas@gmail.com>
2022-11-27 12:33:17 +0900

    バリデーション追加

--- src/components/SampleForm.tsx
+++ src/components/SampleForm.tsx
@@ -14,15 +14,18 @@ const SampleForm = () => {
             <p>一般的なフォーム</p>
             <form>
                 <label>ラベル：</label>
-                <input {...register('label', { pattern: /[a-zA-Z]/ })} />
-                {errors.label && <p>文字を入力してください!</p>}
+                <input {...register('label', { pattern: /a-zA-Z/ })} />
+                {errors.label && <span>文字を入力してください！</span>}
+
                 <label>ラベル：</label>
-                <select {...register('select')}>
+                <select {...register('select', { required: true })}>
                     <option defaultChecked>選択してください</option>
                     <option value='aaa'>aaa</option>
                     <option value='bbb'>bbb</option>
                     <option value='ccc'>ccc</option>
                 </select>
+                {errors.select && <span>どれか1つ以上チェックを入れてください！</span>}
+                
                 <label>ラベル：</label>
                 <input type='checkbox' value='aaa' {...register('checkbox')} />aaa
                 <input type='checkbox' value='bbb' {...register('checkbox')} />bbb
@@ -35,7 +38,8 @@ const SampleForm = () => {
                 </div>
 
                 <label>ラベル：</label>
-                <textarea {...register('textarea')} />
+                <textarea {...register('textarea', { pattern: /a-zA-Z/ })} />
+                {errors.textarea && <span>文字を入力してください！</span>}
 
                 <input type='submit' onClick={handleSubmit(submitForm)} />
             </form>

bc87a322a41b04031459c394254385871d4c7058
azufb <curiousazunas@gmail.com>
2022-11-30 16:23:27 +0900

    axiosClientのexport修正

--- src/axios.ts
+++ src/axios.ts
@@ -1,8 +1,10 @@
 import axios, { AxiosInstance } from "axios";
 
-export const axiosClient: AxiosInstance = axios.create({
+const axiosClient: AxiosInstance = axios.create({
     baseURL: 'https://expresssampleapi.onrender.com',
     headers: {
         'Content-Type': 'application/json'
     }
 });
+
+export default axiosClient;

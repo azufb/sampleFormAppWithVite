9634d4b8b71e85ac0b2c6c595e41b05d691c1b5d
azufb <curiousazunas@gmail.com>
2022-11-30 17:21:40 +0900

    レスポンシブのサンプルのため、ブロック要素を配置

--- src/components/ContentsMenu.tsx
+++ src/components/ContentsMenu.tsx
@@ -4,12 +4,16 @@ import { MenuList, MenuButton } from '../styles/ContentsMenu';
 const ContentsMenu = () => {
     return (
         <MenuList>
-            <MenuButton>
-                <Link to='form'>基本編</Link>
-            </MenuButton>
-            <MenuButton>
-                <Link to='dynamicForm'>動的フォーム</Link>
-            </MenuButton>
+            <div>
+                <MenuButton>
+                    <Link to='form'>基本編</Link>
+                </MenuButton>
+            </div>
+            <div>
+                <MenuButton>
+                    <Link to='dynamicForm'>動的フォーム</Link>
+                </MenuButton>
+            </div>
         </MenuList>
     );
 };

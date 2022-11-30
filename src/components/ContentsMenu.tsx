e5752412311c77f7012daa5467cca7d54df61492
azufb <curiousazunas@gmail.com>
2022-11-30 16:54:38 +0900

    作成したコンポーネントを適用

--- src/components/ContentsMenu.tsx
+++ src/components/ContentsMenu.tsx
@@ -1,11 +1,16 @@
 import { Link } from "react-router-dom";
+import { MenuList, MenuButton } from '../styles/ContentsMenu';
 
 const ContentsMenu = () => {
     return (
-        <div>
-            <Link to='form'>基本編</Link>
-            <Link to='dynamicForm'>動的フォーム</Link>
-        </div>
+        <MenuList>
+            <MenuButton>
+                <Link to='form'>基本編</Link>
+            </MenuButton>
+            <MenuButton>
+                <Link to='dynamicForm'>動的フォーム</Link>
+            </MenuButton>
+        </MenuList>
     );
 };
 

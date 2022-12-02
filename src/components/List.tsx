ac3609b4e20df5893dacaa5960094d76fbcfe863
azufb <curiousazunas@gmail.com>
2022-12-03 00:00:02 +0900

    リストページ作成

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,8 +1,26 @@
+import { useEffect, useState } from "react";
+import axiosClient from "../axios";
+
 const List = () => {
+    const [list, setList] = useState([]);
+
+    useEffect(() => {
+        const getListFunc = async () => {
+            const response = await axiosClient.get('/getTasks');
+            console.log(response);
+
+            setList(response.data);
+        };
+
+        getListFunc();
+    }, []);
+
     return (
         <div>
             <ul>
-                <li>サンプル</li>
+                {list.map((li: any, index: number) => (
+                    <li key={index}>{li.title}</li>
+                ))}
             </ul>
         </div>
     );

a40b356542ab225f004f19aac0f9375e0f3f535f
azufb <curiousazunas@gmail.com>
2022-12-02 22:52:35 +0900

    環境変数の参照修正

--- src/axios.ts
+++ src/axios.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
 const axiosClient: AxiosInstance = axios.create({
-    baseURL: process.env.REACT_APP_API_BASE_URL,
+    baseURL: import.meta.env.VITE_API_BASE_URL,
     headers: {
         'Content-Type': 'application/json'
     }

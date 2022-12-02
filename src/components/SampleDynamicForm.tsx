73c7963521c9115e31e3a43d94155251b5678595
azufb <curiousazunas@gmail.com>
2022-12-02 22:52:53 +0900

    フォーム送信できるように処理作成

--- src/components/SampleDynamicForm.tsx
+++ src/components/SampleDynamicForm.tsx
@@ -1,7 +1,8 @@
 import { useForm, useFieldArray } from "react-hook-form";
+import axiosClient from "../axios";
 
 const SampleDynamicForm = () => {
-    const { register, handleSubmit, control } = useForm({
+    const { register, handleSubmit, control, reset } = useForm({
         defaultValues: {
           sample: [
             {
@@ -14,20 +15,37 @@ const SampleDynamicForm = () => {
         control,
         name: 'sample'
     });
+    
+    const onSubmit = async (data: any) => {
+        for (const task of data.sample) {
+            const param = {
+                title: task.title
+            };
+    
+            console.log(param);
+        
+            const response = await axiosClient.post('/addTask', param);
+            console.log(response);
+        }
+
+        // フォームを空にする。
+        reset();
+    };
 
     return (
         <div>
-            <form>
-            <button type="button" onClick={() => prepend({title: ''})}>先頭に追加</button>
-                {fields.map((field: any, index: number) => (
-                    <div key={field.id}>
-                        <label>タスク：{index}</label>
-                        <input {...register(`sample.${index}.title`)} />
-                        <button type="button" onClick={() => remove(index)}>消去</button>
-                    </div>
-                ))}
-                <button type="button" onClick={() => replace({title: 'You Can Do It!'})}>置き換え</button>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <button type="button" onClick={() => prepend({title: ''})}>先頭に追加</button>
+                    {fields.map((field: any, index: number) => (
+                        <div key={field.id}>
+                            <label>タスク：{index}</label>
+                            <input {...register(`sample.${index}.title`)} />
+                            <button type="button" onClick={() => remove(index)}>消去</button>
+                        </div>
+                    ))}
                 <button type="button" onClick={() => append({title: ''})}>後ろに追加</button>
+
+                <button type="submit">SUBMIT</button>
             </form>
         </div>
     );

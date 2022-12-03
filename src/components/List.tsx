3d7cfacb93e3666317ace57e7437d597b513e3b6
azufb <curiousazunas@gmail.com>
2022-12-04 01:13:25 +0900

    削除処理作成

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import axiosClient from "../axios";
 
 const List = () => {
-    const [list, setList] = useState([]);
+    const [list, setList] = useState<any[]>([]);
 
     useEffect(() => {
         const getListFunc = async () => {
@@ -15,11 +15,29 @@ const List = () => {
         getListFunc();
     }, []);
 
+    const deleteRecord = async (targetId: number) => {
+        const param = {
+            id: targetId
+        }
+        const response = await axiosClient.post('/deleteTask', param);
+        console.log(response);
+        
+        if (response.status === 200) {
+            const getDataResponse = await axiosClient.get('/getTasks');
+            console.log(getDataResponse);
+
+            setList(getDataResponse.data);
+        }
+    };
+
     return (
         <div>
             <ul>
                 {list.map((li: any, index: number) => (
-                    <li key={index}>{li.title}</li>
+                    <div key={index}>
+                        <li key={index}>{li.title}</li>
+                        <button onClick={() => deleteRecord(li.id)}>削除</button>
+                    </div>
                 ))}
             </ul>
         </div>

5da7dbcc19e3a1531bc473685c43cd3bedb8a78e
azufb <curiousazunas@gmail.com>
2022-12-05 11:42:58 +0900

    サンプルフォーム修正

--- src/components/SampleForm.tsx
+++ src/components/SampleForm.tsx
@@ -20,43 +20,50 @@ const SampleForm = () => {
         <div>
             <p>一般的なフォーム</p>
             <form>
-                <label>ラベル：</label>
-                <input {...register('title', { pattern: /[A-Za-z]/ })} />
-                {errors.label && <span>文字を入力してください！</span>}
-
-                <label>ラベル：</label>
-                <select {...register('select', { required: true })}>
-                    <option defaultChecked>選択してください</option>
-                    <option value='aaa'>aaa</option>
-                    <option value='bbb'>bbb</option>
-                    <option value='ccc'>ccc</option>
-                </select>
-                {errors.select && <span>どれか1つ以上チェックを入れてください！</span>}
-                
-                <label>ラベル：</label>
-                <input type='checkbox' value='aaa' {...register('checkbox')} />aaa
-                <input type='checkbox' value='bbb' {...register('checkbox')} />bbb
-                <input type='checkbox' value='ccc' {...register('checkbox')} />ccc
-
-                <label>ラベル：</label>
-                <div radioGroup="rg">
-                    <input type='radio' value='yes' {...register('rg')} defaultChecked />YES
-                    <input type='radio' value='no' {...register('rg')} />NO
-                </div>
-
-                <label>ラベル：</label>
-                <textarea {...register('textarea', { pattern: /[A-Za-z]/ })} />
-                {errors.textarea && <span>文字を入力してください！</span>}
-
-                <input type='submit' onClick={handleSubmit(submitForm)} />
+                <label>タスク：</label>
+                <input {...register('title')} />
+                <button type="submit" onClick={handleSubmit(submitForm)}>SUBMIT</button>
             </form>
-            <div>
-                <p>label：{watchAllVal.label}</p>
-                <p>select：{watchAllVal.select}</p>
-                <p>checkbox：{watchAllVal.checkbox}</p>
-                <p>radio：{watchAllVal.rg}</p>
-                <p>textarea：{watchAllVal.textarea}</p>
-            </div>
+            {/*
+                <form>
+                    <label>ラベル：</label>
+                    <input {...register('title', { pattern: /[A-Za-z]/ })} />
+                    {errors.label && <span>文字を入力してください！</span>}
+
+                    <label>ラベル：</label>
+                    <select {...register('select', { required: true })}>
+                        <option defaultChecked>選択してください</option>
+                        <option value='aaa'>aaa</option>
+                        <option value='bbb'>bbb</option>
+                        <option value='ccc'>ccc</option>
+                    </select>
+                    {errors.select && <span>どれか1つ以上チェックを入れてください！</span>}
+                    
+                    <label>ラベル：</label>
+                    <input type='checkbox' value='aaa' {...register('checkbox')} />aaa
+                    <input type='checkbox' value='bbb' {...register('checkbox')} />bbb
+                    <input type='checkbox' value='ccc' {...register('checkbox')} />ccc
+
+                    <label>ラベル：</label>
+                    <div radioGroup="rg">
+                        <input type='radio' value='yes' {...register('rg')} defaultChecked />YES
+                        <input type='radio' value='no' {...register('rg')} />NO
+                    </div>
+
+                    <label>ラベル：</label>
+                    <textarea {...register('textarea', { pattern: /[A-Za-z]/ })} />
+                    {errors.textarea && <span>文字を入力してください！</span>}
+
+                    <input type='submit' onClick={handleSubmit(submitForm)} />
+                </form>
+                <div>
+                    <p>label：{watchAllVal.label}</p>
+                    <p>select：{watchAllVal.select}</p>
+                    <p>checkbox：{watchAllVal.checkbox}</p>
+                    <p>radio：{watchAllVal.rg}</p>
+                    <p>textarea：{watchAllVal.textarea}</p>
+                </div>
+            */}
         </div>
     );
 };

c4cd1c630c989a3a3ddfe645e5fc96ca3fa40404
azufb <curiousazunas@gmail.com>
2023-06-06 17:15:16 +0900

    ボタンの文字色修正

--- src/styles/ContentsMenu.ts
+++ src/styles/ContentsMenu.ts
@@ -1,42 +1,38 @@
 import { styled } from './stitches.config'; // configを作成しているので、@stitches/reactからではなく、configからimport。
 
 const MenuList = styled('div', {
-    '@tablet': {
-        display: 'block'
-    },
-    '@pc': {
-        display: 'flex',
-        justifyContent: 'center',
-        columnGap: '1rem',
-    }
+  '@tablet': {
+    display: 'block',
+  },
+  '@pc': {
+    display: 'flex',
+    justifyContent: 'center',
+    columnGap: '1rem',
+  },
 });
 
 const MenuButtonWrapDiv = styled('div', {
-    '@tablet': {
-        marginBottom: '1rem'
-    },
-    '@pc': {
-        marginBottom: '0'
-    }
+  '@tablet': {
+    marginBottom: '1rem',
+  },
+  '@pc': {
+    marginBottom: '0',
+  },
 });
 
 const MenuButton = styled('button', {
-    width: '10rem',
-    height: '3rem',
-    padding: '0.5rem 1rem',
-    borderRadius: '15px',
-    backgroundColor: '$menuButtonBG',
+  width: '10rem',
+  height: '3rem',
+  padding: '0.5rem 1rem',
+  borderRadius: '15px',
+  backgroundColor: '$menuButtonBG',
+  color: '$colorWhite',
+  '& a': {
+    textDecoration: 'none',
+  },
+  '& a:visited': {
     color: '$colorWhite',
-    '& a': {
-        textDecoration: 'none'
-    },
-    '& a:visited': {
-        color: 'inherit'
-    }
+  },
 });
 
-export {
-    MenuList,
-    MenuButtonWrapDiv,
-    MenuButton
-}
+export { MenuList, MenuButtonWrapDiv, MenuButton };

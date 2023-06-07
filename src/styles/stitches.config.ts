77775e62629e127357130e0512147905631e0a91
azufb <curiousazunas@gmail.com>
2023-06-07 21:37:45 +0900

    背景色追加

--- src/styles/stitches.config.ts
+++ src/styles/stitches.config.ts
@@ -1,18 +1,19 @@
 import { createStitches } from '@stitches/react';
 
 export const { styled, css } = createStitches({
-    media: {
-        // ブレークポイントは、Bootstrap参照。(https://getbootstrap.jp/docs/5.0/layout/breakpoints/)
-        phone: '(min-width: 576px)',
-        tablet: '(min-width: 768px)',
-        pc: '(min-width: 992px)',
+  media: {
+    // ブレークポイントは、Bootstrap参照。(https://getbootstrap.jp/docs/5.0/layout/breakpoints/)
+    phone: '(min-width: 576px)',
+    tablet: '(min-width: 768px)',
+    pc: '(min-width: 992px)',
+  },
+  theme: {
+    colors: {
+      menuButtonBG: '#636DFF',
+      submitButtonBG: '#0f83fd',
+      successBG: '#58CA5E',
+      fontColorDefault: '#2F2F2F',
+      colorWhite: '#FFFFFF',
     },
-    theme: {
-        colors: {
-            menuButtonBG: '#636DFF',
-            submitButtonBG: '#0f83fd',
-            fontColorDefault: '#2F2F2F',
-            colorWhite: '#FFFFFF'
-        }
-    }
+  },
 });

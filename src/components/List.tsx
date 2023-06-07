8f8d13193cb8a5f9de123badb420eee331631888
azufb <curiousazunas@gmail.com>
2023-06-07 21:10:02 +0900

    スタイル調整

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,64 +1,85 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useEffect, useState } from "react";
-import axiosClient from "../axios";
-import { faTrashCan } from "@fortawesome/free-regular-svg-icons";
-import { Title, AlternativeText, ListArea } from "../styles/List";
-import { AxiosResponse } from "axios";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useEffect, useState } from 'react';
+import axiosClient from '../axios';
+import { faTrashCan } from '@fortawesome/free-regular-svg-icons';
+import {
+  ListPageWrapper,
+  Title,
+  AlternativeText,
+  ListArea,
+  DeleteBtnIcon,
+  BackToHomeBtnArea,
+} from '../styles/List';
+import { AxiosResponse } from 'axios';
+import BackToHomeButton from './BackToHomeButton';
 
 type DeleteRecordParamType = {
-    id: number;
+  id: number;
 };
 
 const List = () => {
-    const [list, setList] = useState<any[]>([]);
+  const [list, setList] = useState<any[]>([]);
 
-    useEffect(() => {
-        const getListFunc = async (): Promise<void> => {
-            const response: AxiosResponse<any> = await axiosClient.get('/getTasks');
-            console.log(response);
+  useEffect(() => {
+    const getListFunc = async (): Promise<void> => {
+      const response: AxiosResponse<any> = await axiosClient.get('/getTasks');
+      console.log(response);
 
-            setList(response.data);
-        };
-
-        getListFunc();
-    }, []);
-
-    const deleteRecord = async (targetId: number): Promise<void> => {
-        const param: DeleteRecordParamType = {
-            id: targetId
-        }
-        const response: AxiosResponse<any> = await axiosClient.post('/deleteTask', param);
-        console.log(response);
-        
-        if (response.status === 200) {
-            const getDataResponse: AxiosResponse<any> = await axiosClient.get('/getTasks');
-            console.log(getDataResponse);
-
-            setList(getDataResponse.data);
-        }
+      setList(response.data);
     };
 
-    return (
-        <div>
-            <Title>一覧</Title>
-            <ListArea>
-                {list.length === 0 ? (
-                    <AlternativeText>登録されているタスクは0件です。</AlternativeText>
-                ) : (
-                        <ul>
-                            {list.map((li: any, index: number) => (
-                                <div key={index}>
-                                    <li key={index}>
-                                        {li.title}
-                                        <FontAwesomeIcon icon={faTrashCan} onClick={() => deleteRecord(li.id)} />
-                                    </li>
-                                </div>
-                            ))}
-                        </ul>
-                )}
-            </ListArea>
-        </div>
+    getListFunc();
+  }, []);
+
+  const deleteRecord = async (targetId: number): Promise<void> => {
+    const param: DeleteRecordParamType = {
+      id: targetId,
+    };
+    const response: AxiosResponse<any> = await axiosClient.post(
+      '/deleteTask',
+      param
     );
+    console.log(response);
+
+    if (response.status === 200) {
+      const getDataResponse: AxiosResponse<any> = await axiosClient.get(
+        '/getTasks'
+      );
+      console.log(getDataResponse);
+
+      setList(getDataResponse.data);
+    }
+  };
+
+  return (
+    <ListPageWrapper>
+      <Title>一覧</Title>
+      <ListArea>
+        {list.length === 0 ? (
+          <AlternativeText>登録されているタスクは0件です。</AlternativeText>
+        ) : (
+          <ul>
+            {list.map((li: any, index: number) => (
+              <div key={index}>
+                <li key={index}>
+                  {li.title}
+                  <DeleteBtnIcon>
+                    <FontAwesomeIcon
+                      icon={faTrashCan}
+                      onClick={() => deleteRecord(li.id)}
+                    />
+                  </DeleteBtnIcon>
+                </li>
+              </div>
+            ))}
+          </ul>
+        )}
+      </ListArea>
+      <BackToHomeBtnArea>
+        <BackToHomeButton />
+      </BackToHomeBtnArea>
+    </ListPageWrapper>
+  );
 };
 
 export default List;
--- src/styles/List.ts
+++ src/styles/List.ts
@@ -1,31 +1,50 @@
-import { styled } from "./stitches.config";
+import { styled } from './stitches.config';
+
+const ListPageWrapper = styled('div', {
+  padding: '2.5rem 0',
+});
 
 const Title = styled('h1', {
-    color: '$fontColorDefault',
-    '@tablet': {
-        textAlign: 'left'
-    },
-    '@pc': {
-        textAlign: 'center'
-    }
+  margin: '0',
+  color: '$fontColorDefault',
+  '@tablet': {
+    textAlign: 'left',
+  },
+  '@pc': {
+    textAlign: 'center',
+  },
 });
 
 const AlternativeText = styled('p', {
-    color: '$fontColorDefault'
+  color: '$fontColorDefault',
 });
 
 const ListArea = styled('div', {
-    position: 'absolute',
-    left: '0',
-    right: '0',
-    width: '70%',
-    height: '80%',
-    margin: '0 auto',
-    border: '2px solid $fontColorDefault'
+  position: 'absolute',
+  left: '0',
+  right: '0',
+  width: '70%',
+  margin: '0 auto',
+});
+
+const DeleteBtnIcon = styled('span', {
+  paddingLeft: '0.5rem',
+});
+
+const BackToHomeBtnArea = styled('div', {
+  position: 'absolute',
+  bottom: '2.5rem',
+  margin: '0 auto',
+  left: '0px',
+  right: '0px',
+  textAlign: 'center',
 });
 
 export {
-    Title,
-    AlternativeText,
-    ListArea
+  ListPageWrapper,
+  Title,
+  AlternativeText,
+  ListArea,
+  DeleteBtnIcon,
+  BackToHomeBtnArea,
 };
